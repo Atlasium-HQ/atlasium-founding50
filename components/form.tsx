2a315f30884b0d88ba508e8b91e9ea6028a7fa5a
Enoch Kambale <campnoch@gmail.com>
2025-09-19 02:07:05 +0300

    fix: fix github link

--- components/form.tsx
+++ components/form.tsx
@@ -69,7 +69,7 @@ export default function Form({
         </Link>
         or
         <Link
-          href="https://github.com/AtlasiumHQ"
+          href="https://github.com/Atlasium-HQ"
           rel="noopener noreferrer"
           target="_blank">
           <FaGithub className="hover:[#D4AF37] ml-0.5 h-5 w-5 transition-all duration-200 ease-linear" />
